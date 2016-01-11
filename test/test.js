6db2960dd5628c3b924ed208d8082b88b6b4945d
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2016-01-11 23:22:01 +0800

    :smile:

--- test/test.js
+++ test/test.js
@@ -6,8 +6,7 @@ const os = require('os');
 const execSync = require('child_process').execSync;
 
 describe('check-branch', () => {
-  console.log(execSync('git status').toString());
-  const currentBranch = execSync('git status').toString().match(/^On\sbranch\s(.*)\s/)[1];
+  const currentBranch = (execSync('git status').toString().match(/^On\sbranch\s(.*)\s/) || [])[1] || 'master';
   before(() => execSync('git checkout -b testbranch', {
     stdio: 'pipe'
   }));
